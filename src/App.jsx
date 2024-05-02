bdc48b099c7de4b68c71f58388ef4b044f7ddd41
Kgl-01 <karthikgowda.mailme@gmail.com>
2024-05-03 04:53:26 +0530

    rendered filtered dropdown for multiple scenarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import BoltIcon from "@mui/icons-material/Bolt"
 import {
   Avatar,
+  Box,
   Button,
   Card,
   CardActions,
@@ -9,6 +10,8 @@ import {
   Chip,
   Container,
   Grid,
+  Skeleton,
+  Stack,
   Typography,
   styled,
 } from "@mui/material"
@@ -34,17 +37,23 @@ const joinStrings = (words) => {
 
 function App() {
   const [jobList, setJobList] = useState([])
-
+  const [loading, setLoading] = useState(false)
   const fetchJobLists = async () => {
+    setLoading(true)
     try {
       const response = await axios.post(
         "https://api.weekday.technology/adhoc/getSampleJdJSON",
         { limit: 10, offset: 0 }
       )
-
-      setJobList(response.data?.jdList)
+      if (response.status === 200) {
+        setJobList(response.data?.jdList)
+        return
+      }
+      return Promise.reject(response)
     } catch (error) {
       console.log(error)
+    } finally {
+      setLoading(false)
     }
   }
 
@@ -53,145 +62,200 @@ function App() {
   }, [])
 
   return (
-    <Container maxWidth="lg" style={{ padding: "4rem 2rem" }}>
-      <Grid container></Grid>
+    <Container
+      maxWidth="lg"
+      sx={{
+        padding: "4rem 2rem",
+        display: "flex",
+        flexDirection: "column",
+        gap: "3rem",
+      }}
+    >
+      <Grid container display={"flex"} justifyContent="space-between">
+        <Grid item>
+          <Select placeholder="Min. Experience" />
+        </Grid>
+        <Grid item>
+          <Select placeholder="Company Name" />
+        </Grid>
+        <Grid item>
+          <Select placeholder="Location" />
+        </Grid>
+        <Grid item>
+          <Select placeholder="Remote/Onsite" />
+        </Grid>
+        <Grid item>
+          <Select placeholder="Tech Stack" />
+        </Grid>
+        <Grid item>
+          <Select placeholder="Role" />
+        </Grid>
+        <Grid item>
+          <Select placeholder="Min Base Pay Salary" />
+        </Grid>
+      </Grid>
 
       <Grid container spacing={8}>
-        {jobList
-          ? jobList?.map((job, index) => (
-              <Grid item xs={12} lg={4} md={4} sm={6} key={job.jdUid}>
-                <Card
+        {!loading ? (
+          jobList?.map((job, index) => (
+            <Grid item xs={12} lg={4} md={4} sm={6} key={job.jdUid}>
+              <Card
+                sx={{
+                  width: "100%",
+                  boxShadow: "none",
+                  filter: "drop-shadow(0rem 0rem 0.25rem #00000025)",
+                  borderRadius: "1rem",
+                  transform: "scale(1)",
+                  transition: "all 0.1s ease-out",
+                  padding: "1rem",
+                  "&:hover": {
+                    transform: "scale(1.01)",
+                    transition: "all cubic-bezier(0.4, 0, 0.2, 1) 0.1s ease-in",
+                  },
+                }}
+              >
+                <Chip
+                  label="⏳ Posted 10 days ago"
                   sx={{
-                    width: "100%",
-                    boxShadow: "none",
-                    filter: "drop-shadow(0rem 0rem 0.25rem #00000025)",
-                    borderRadius: "1rem",
-                    transition: "all 0.1s ease-out",
-                    padding: "1rem",
-                    "&:hover": {
-                      transform: "scale(1.01)",
-                      transition: "all 0.1s ease-in",
-                    },
+                    background: "transparent",
+                    boxShadow: "0rem 0rem 0.5rem #00000040",
                   }}
-                >
-                  <Chip
-                    label="⏳ Posted 10 days ago"
-                    sx={{
-                      background: "transparent",
-                      boxShadow: "0rem 0rem 0.5rem #00000040",
-                    }}
-                    size="small"
-                  />
-                  <CardHeader
-                    avatar={
-                      <Avatar aria-label="image">
-                        {job.location[0].toUpperCase()}
-                      </Avatar>
-                    }
-                    title={
-                      <div
+                  size="small"
+                />
+                <CardHeader
+                  avatar={
+                    <Avatar aria-label="image">
+                      {job.location[0].toUpperCase()}
+                    </Avatar>
+                  }
+                  title={
+                    <div
+                      style={{
+                        padding: "0rem",
+                        margin: "0rem",
+                        fontWeight: "500",
+                        color: "#8b8b8b",
+                        fontSize: "0.9rem",
+                      }}
+                    >
+                      Week Day
+                      <p
                         style={{
                           padding: "0rem",
                           margin: "0rem",
                           fontWeight: "500",
-                          color: "#8b8b8b",
-                          fontSize: "0.9rem",
+                          color: "#000",
                         }}
                       >
-                        Week Day
-                        <p
-                          style={{
-                            padding: "0rem",
-                            margin: "0rem",
-                            fontWeight: "500",
-                            color: "#000",
-                          }}
-                        >
-                          {joinStrings(job.jobRole)}
-                        </p>
-                      </div>
-                    }
-                    subheader={
-                      <small>{capitalizeFirstLetter(job.location)}</small>
-                    }
-                  />
-
-                  <CardContent sx={{ position: "relative" }}>
-                    <Typography>
-                      Estimated Salary: $
-                      {job.minJdSalary && `${job.minJdSalary}k`}
-                      {job.minJdSalary && job?.maxJdSalary ? " - " : ""}
-                      {`${job.maxJdSalary}k`} USD
-                    </Typography>
-                    <Typography variant="h6">About Company:</Typography>
-                    <div
-                      style={{
-                        overflow: "auto",
-                        height: "20.5rem",
-                        scrollbarWidth: "none",
-                        pointerEvents: "none",
-                      }}
-                    >
-                      {job.jobDetailsFromCompany}
+                        {joinStrings(job.jobRole)}
+                      </p>
                     </div>
-                    <Button
-                      sx={{
-                        width: "100%",
-                        background:
-                          "linear-gradient(to bottom,rgba(255,255,255,0.9), white)",
-                        position: "absolute",
-                        left: "0",
-                        bottom: "0",
-                        height: "3rem",
-                        textTransform: "none",
-                        boxShadow: "0rem -2rem 2rem rgba(255,255,255,1)",
-                        "&:hover": {
-                          textDecoration: "underline",
-                        },
-                      }}
-                      disableRipple
-                    >
-                      Show more
-                    </Button>
-                  </CardContent>
-                  <CardActions>
-                    <Grid container spacing={3}>
-                      <Grid item display={"flex"} flexDirection={"column"}>
-                        <Typography variant="h7" fontWeight={600}>
-                          Minimum Experience
-                        </Typography>
-                        <Typography variant="h8">
-                          {job.minExp ? `${job.minExp} years` : "Any"}
-                        </Typography>
-                      </Grid>
-                      <Grid item xs={12}>
-                        <Button
-                          startIcon={<CustomBoltIcon />}
-                          variant="contained"
-                          sx={{
+                  }
+                  subheader={
+                    <small>{capitalizeFirstLetter(job.location)}</small>
+                  }
+                />
+
+                <CardContent sx={{ position: "relative" }}>
+                  <Typography>
+                    Estimated Salary: $
+                    {job.minJdSalary && `${job.minJdSalary}k`}
+                    {job.minJdSalary && job?.maxJdSalary ? " - " : ""}
+                    {`${job.maxJdSalary}k`} USD
+                  </Typography>
+                  <Typography variant="h6">About Company:</Typography>
+                  <div
+                    style={{
+                      overflow: "auto",
+                      height: "20.5rem",
+                      scrollbarWidth: "none",
+                      pointerEvents: "none",
+                    }}
+                  >
+                    {job.jobDetailsFromCompany}
+                  </div>
+                  <Button
+                    sx={{
+                      width: "100%",
+                      background:
+                        "linear-gradient(to bottom,rgba(255,255,255,0.9), white)",
+                      position: "absolute",
+                      left: "0",
+                      bottom: "0",
+                      height: "3rem",
+                      textTransform: "none",
+                      boxShadow: "0rem -2rem 2rem rgba(255,255,255,1)",
+                      "&:hover": {
+                        textDecoration: "underline",
+                      },
+                    }}
+                    disableRipple
+                  >
+                    Show more
+                  </Button>
+                </CardContent>
+                <CardActions>
+                  <Grid container spacing={3}>
+                    <Grid item display={"flex"} flexDirection={"column"}>
+                      <Typography variant="h7" fontWeight={600}>
+                        Minimum Experience
+                      </Typography>
+                      <Typography variant="h8">
+                        {job.minExp ? `${job.minExp} years` : "Any"}
+                      </Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                      <Button
+                        startIcon={<CustomBoltIcon />}
+                        variant="contained"
+                        sx={{
+                          background: "#55EFC4",
+                          boxShadow: "none",
+                          width: "100%",
+                          color: "#000",
+                          textTransform: "none",
+                          fontSize: "1rem",
+                          borderRadius: "0.5rem",
+                          "&:hover": {
                             background: "#55EFC4",
                             boxShadow: "none",
-                            width: "100%",
-                            color: "#000",
-                            textTransform: "none",
-                            fontSize: "1rem",
-                            borderRadius: "0.5rem",
-                            "&:hover": {
-                              background: "#55EFC4",
-                              boxShadow: "none",
-                            },
-                          }}
-                          onClick={() => window.open(job.jdLink, "_blank")}
-                        >
-                          Easy Apply
-                        </Button>
-                      </Grid>
+                          },
+                        }}
+                        onClick={() => window.open(job.jdLink, "_blank")}
+                      >
+                        Easy Apply
+                      </Button>
                     </Grid>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))
-          : "Loading..."}
+                  </Grid>
+                </CardActions>
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          <Grid item xs={12} lg={4} md={4} sm={6}>
+            <Box sx={{ width: "100%", height: "1rem" }}>
+              <Box sx={{ width: "100%", display: "flex", gap: "1rem" }}>
+                <Skeleton variant="circular" sx={{ aspectRatio: "1/1" }}>
+                  <Avatar />
+                </Skeleton>
+                <Stack sx={{ width: "100%" }}>
+                  {Array.from({ length: 3 }, (_, i) => (
+                    <Skeleton height={14} width={100} />
+                  ))}
+                </Stack>
+
+                <Stack sx={{ width: "100%" }}>
+                  <Skeleton height={18} width="100%" />
+                  {Array.from({ length: 10 }, (_, i) => (
+                    <Skeleton height={14} width="100%" />
+                  ))}
+                </Stack>
+                <Skeleton variant="rectangular" height="3rem" width="100%" />
+              </Box>
+              <Stack></Stack>
+            </Box>
+          </Grid>
+        )}
       </Grid>
     </Container>
   )
